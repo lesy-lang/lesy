0d3bb77b0f3de18862993c547bbf004d26f205e9
passi <pascalfeyer@gmail.com>
2020-11-19 17:04:50 +0100

    (+ tests)

---
+++ browser/tests/Example.elm
@@ -0,0 +1,12 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+--see https://package.elm-lang.org/packages/elm-explorations/test/latest
+suite: Test
+suite=
+  todo "Implement our first test."
+
